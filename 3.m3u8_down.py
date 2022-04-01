bb305783bd7042552b70726650f14a7af62606fd
Zkeq <learnonly@learnonly.xyz>
2022-04-02 01:49:27 +0800

    url编码

--- 3.m3u8_down.py
+++ 3.m3u8_down.py
@@ -1,14 +1,17 @@
 # coding: utf-8
+import string
 import redis
 import os
-import psutil
+from urllib.parse import quote
+
 
 r = redis.Redis(host='localhost', port=6379)
 
 while True:
     url = r.blpop('m3u8_url')[1].decode('utf-8')
     print("获取到m3u8地址: ", url)
-    pl = psutil.pids()
+    url = quote(url, safe=string.printable)
+    print("转码后的m3u8地址: ", url)
     print("正在下载 m3u8 文件", url)
     os.system("edu_video.exe" + " " + url)
     print("该文件下载完成！")
