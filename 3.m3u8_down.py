8220a90daddca4819bd758a24f20a01f211d6fcc
Zkeq <learnonly@learnonly.xyz>
2022-04-02 12:22:05 +0800

    [BUG] m3u8 空格转义

--- 3.m3u8_down.py
+++ 3.m3u8_down.py
@@ -10,7 +10,7 @@ r = redis.Redis(host='localhost', port=6379)
 while True:
     url = r.blpop('m3u8_url')[1].decode('utf-8')
     print("获取到m3u8地址: ", url)
-    url = quote(url, safe=string.printable)
+    url = quote(url, safe="/:?=&%20")
     work_dir = os.getcwd() + r.get('work_dir').decode('utf-8').replace('/', '\\')
     print("转码后的m3u8地址: ", url)
     print("正在下载 m3u8 文件", url)
