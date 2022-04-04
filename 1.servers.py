0f6ca08356de387aebb3b41ff298e74deecce5ee
Zkeq <learnonly@learnonly.xyz>
2022-04-04 10:58:56 +0800

    [优化] 优化部分代码逻辑

--- 1.servers.py
+++ 1.servers.py
@@ -6,8 +6,7 @@ r = redis.Redis(host='localhost', port=6379)
 
 while True:
     m3u8_sig = r.blpop('queue')
-    sig_list = m3u8_sig[1].decode(encoding='utf-8', errors='strict').split(',')
-    for i in sig_list:
-        r.lpush('html_url', i)
-        print("成功推送UID", i)
+    sig_list = m3u8_sig[1].decode(encoding='utf-8', errors='strict')
+    r.lpush('html_url', sig_list)
+    print("成功推送UID", sig_list)
     print("推送完成")
